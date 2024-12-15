afc71a06410547fcc0fc840512becbdb4311df22
Vlad <vladfors@gmail.com>
2024-12-16 00:44:01 +0400

    sprint 2 Check manager devices

--- microservices/management-device/src/main/java/ru/yandex/practicum/device/kafka/producer/DeviceStatusPublisher.java
+++ microservices/management-device/src/main/java/ru/yandex/practicum/device/kafka/producer/DeviceStatusPublisher.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.device.kafka.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.device.dto.DeviceStatus;
@@ -9,6 +10,7 @@ import ru.yandex.practicum.device.dto.DeviceStatus;
 @Service
 public class DeviceStatusPublisher {
 
+    @Autowired
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
 
