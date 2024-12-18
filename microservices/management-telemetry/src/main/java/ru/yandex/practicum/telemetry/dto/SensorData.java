c79c8b7cc0c3c1f38937960edfd60c2279448037
Vlad <vladfors@gmail.com>
2024-12-19 00:56:37 +0400

    Fixed bug kafka

--- microservices/management-telemetry/src/main/java/ru/yandex/practicum/telemetry/dto/SensorData.java
+++ microservices/management-telemetry/src/main/java/ru/yandex/practicum/telemetry/dto/SensorData.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Setter
 @Getter
 
 public class SensorData extends Common {
 
-    private Integer deviceId;
+    private UUID deviceId;
     private Double temperature;
     private Double humidity;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
